af04ff03d5766025762423cef77f8340d549bd7a
郑建立 <2425228686@qq.com>
2018-11-15 20:46:37 +0800

    fixed 店铺装修的bug2

--- src/views/accsetting/index.vue
+++ src/views/accsetting/index.vue
@@ -10,101 +10,25 @@
             <el-form-item label="用户名">
               <el-input :value="username" :disabled="true" type="text" readonly/>
             </el-form-item>
-          </el-col>
-
-          <el-col :md="12" :sm="24">
             <el-form-item label="账户状态">
               <el-input :value="useraccount" :disabled="true" type="text" readonly/>
             </el-form-item>
-          </el-col>
-
-          <el-col :md="12" :sm="24">
             <el-form-item label="店铺名称">
               <el-input :value="shopname" :disabled="true" type="text" readonly/>
             </el-form-item>
-          </el-col>
-
-          <el-col :md="12" :sm="24">
             <el-form-item label="企业类型">
               <el-input :value="gongsi" :disabled="true" type="text" readonly/>
-            </el-form-item>
-          </el-col>
-
-          <el-col :md="12" :sm="24">
-            <el-form-item label="位置：">
-              <el-input v-model="inputaddress.zh_CN" :disabled="true" class="myinput" readonly/>
-            </el-form-item>
-          </el-col>
-
-          <el-col :md="12" :sm="24">
-            <el-form-item
-              :rules="[{ required: true, message: '生产能力不能为空',trigger:'blur'}]"
-              label="生产能力："
-              prop="inputpower.zh_CN">
-              <el-input v-model="form.inputpower.zh_CN" class="myinput"/>
-            </el-form-item>
-          </el-col>
-
-          <el-col :md="12" :sm="24">
-            <el-form-item
-              :rules="[{ required: true, message: '生产总数不能为空',trigger:'blur'}]"
-              label="生产总数："
-              prop="inputnums.zh_CN">
-              <el-input v-model="form.inputnums.zh_CN" class="myinput" placeholder="请输入整数"/>
-            </el-form-item>
-          </el-col>
-          <el-col :md="12" :sm="24">
-            <el-form-item
-              :rules="[{ required: true, message: '员工总数不能为空',trigger:'blur'}]"
-              label="员工总数："
-              prop="inputstaffs.zh_CN">
-              <el-input v-model="form.inputstaffs.zh_CN" class="myinput" placeholder="请输入整数"/>
-            </el-form-item>
-          </el-col>
-
-          <el-col :md="12" :sm="24">
-            <el-form-item
-              :rules="[{ required: true, message: '年销售量不能为空',trigger:'blur'}]"
-              label="年销售量："
-              prop="inputyearnum.zh_CN">
-              <el-input v-model="form.inputyearnum.zh_CN" class="myinput" placeholder="请输入整数"/>
-            </el-form-item>
-          </el-col>
-        </el-row>
-
-        <el-row :gutter="gutter">
-          <el-col :md="12" :sm="24">
-            <el-form-item
-              :rules="[{ required: true, message: '第三市场不能为空',trigger:'blur'}]"
-              label="第三市场："
-              prop="textmarket.zh_CN">
-              <el-input v-model="form.textmarket.zh_CN" class="mytextarea" type="textarea" rows="5" resize="none"/>
-            </el-form-item>
-          </el-col>
-
-          <el-col :md="12" :sm="24">
-            <el-form-item
-              :rules="[{ required: true, message: '材料选择不能为空',trigger:'blur'}]"
-              label="材料选择："
-              prop="texttype.zh_CN">
-              <el-input v-model="form.texttype.zh_CN" class="mytextarea" type="textarea" rows="5" resize="none"/>
             </el-form-item>
           </el-col>
         </el-row>
         <el-row>
           <el-col :md="12" :sm="24">
-            <el-form-item label="头像：">
-              <el-upload
-                :show-file-list="false"
-                :on-success="userSuccess"
-                class="avatar-uploader"
-                action="/seller/usr_UsrSupplier_upload">
-                <img v-if="userimageUrl" :src="image(userimageUrl)" class="avatar img-contain">
-                <i v-else class="el-icon-plus avatar-uploader-icon"/>
-              </el-upload>
+            <el-form-item label="最后登录">
+              <el-input :value="lastlogin" :disabled="true" type="text" readonly/>
             </el-form-item>
           </el-col>
-
+        </el-row>
+        <el-row>
           <el-col :md="12" :sm="24">
             <el-form-item label="LOGO：">
               <el-upload
@@ -118,41 +42,6 @@
             </el-form-item>
           </el-col>
         </el-row>
-        <el-row>
-          <el-col :md="12" :sm="24">
-            <el-form-item
-              :rules="[{ required: true, message: '部门不能为空',trigger:'blur'}]"
-              label="部门："
-              prop="department.zh_CN">
-              <el-input v-model="form.department.zh_CN" class="myinput"/>
-            </el-form-item>
-          </el-col>
-
-          <el-col :md="12" :sm="24">
-            <el-form-item
-              :rules="[{ required: true, message: '职称不能为空',trigger:'blur'}]"
-              label="职称："
-              prop="inputtitle.zh_CN">
-              <el-input v-model="form.inputtitle.zh_CN" class="myinput"/>
-            </el-form-item>
-          </el-col>
-
-          <el-col :md="12" :sm="24">
-            <el-form-item
-              :rules="[{ required: true, message: '网站不能为空',trigger:'blur'}]"
-              label="网站："
-              prop="inputweb">
-              <el-input v-model="form.inputweb" class="myinput"/>
-            </el-form-item>
-          </el-col>
-
-          <el-col :md="12" :sm="24">
-            <el-form-item label="最后登录">
-              <el-input :value="lastlogin" :disabled="true" type="text" readonly/>
-            </el-form-item>
-          </el-col>
-        </el-row>
-        <el-button type="primary" style="margin-left: 47.5%" @click="update">修改</el-button>
       </el-form>
     </el-card>
 
@@ -247,20 +136,20 @@ export default {
         }
         data = data.result
         that.logourl = data.logo
-        that.userimageUrl = data.headPic
-        that.form.inputweb = data.website
+        // that.userimageUrl = data.headPic
+        // that.form.inputweb = data.website
         that.username = data.loginName
         that.shopname = data.name
         that.gongsi = data.companyType
-        that.inputaddress = JSON.parse(data.location)
-        that.form.inputpower = JSON.parse(data.production)
-        that.form.inputnums = JSON.parse(data.developer)
-        that.form.inputstaffs = JSON.parse(data.totalEmployees)
-        that.form.inputyearnum = JSON.parse(data.annualSales)
-        that.form.textmarket = JSON.parse(data.top3Markets)
-        that.form.texttype = JSON.parse(data.materials)
-        that.form.department = JSON.parse(data.department)
-        that.form.inputtitle = JSON.parse(data.jobTitle)
+        // that.inputaddress = JSON.parse(data.location)
+        // that.form.inputpower = JSON.parse(data.production)
+        // that.form.inputnums = JSON.parse(data.developer)
+        // that.form.inputstaffs = JSON.parse(data.totalEmployees)
+        // that.form.inputyearnum = JSON.parse(data.annualSales)
+        // that.form.textmarket = JSON.parse(data.top3Markets)
+        // that.form.texttype = JSON.parse(data.materials)
+        // that.form.department = JSON.parse(data.department)
+        // that.form.inputtitle = JSON.parse(data.jobTitle)
       }
     }
     ).catch(function(error) {
@@ -268,90 +157,28 @@ export default {
     })
   },
   methods: {
-    image(v) {
-      return store.getters.ImageBaseUrl + v
-    },
-    userSuccess(res, file) {
-      if (res.ret === 0) {
-        alert(res.msg)
-      } else {
-        this.userimageUrl = res.result.url
-      }
-    },
     handleAvatarSuccess(res, file) {
-      if (res.ret === 0) {
-        alert(res.msg)
-      } else {
+      if (res.ret === 1) {
         this.logourl = res.result.url
+        console.log({
+          logo: res.result.url
+        })
+        request({
+          method: 'post',
+          url: '/seller/usr_UsrSupplier_updLogo',
+          data: {
+            logo: res.result.url
+          }
+        })
+      } else {
+        alert(res.msg)
       }
-      // if (res.ret === 1) {
-      //   request({
-      //     method: 'post',
-      //     url: '/seller/usr_UsrSupplier_updLogo',
-      //     data: {
-      //       'logo': this.logourl,
-      //       'bean.rowVersion': ''
-      //     }
-      //   }).then(function(res) {
-      //     console.log(res)
-      //   })
-      // }
-    },
-    update() {
-      const self = this
-      self.$refs.editInfoForm.validate(function(e) {
-        if (!e) {
-          self.$message({
-            message: '请将表单填写完整',
-            type: 'error'
-          })
-        } else {
-          request({
-            url: '/seller/usr_UsrSupplier_upAccount',
-            method: 'post',
-            data: {
-              'asv.productionCapacity': JSON.stringify(self.form.inputpower),
-              'asv.totalProduction':
-                JSON.stringify(self.form.inputnums),
-              'asv.numberEmployees':
-                JSON.stringify(self.form.inputstaffs),
-              'asv.annualSalesFigure':
-                JSON.stringify(self.form.inputyearnum),
-              'asv.thirdMarket':
-                JSON.stringify(self.form.textmarket),
-              'asv.materialSelection':
-                JSON.stringify(self.form.texttype),
-              'asv.avatar':
-              self.userimageUrl,
-              'asv.logo':
-              self.logourl,
-              'asv.department':
-                JSON.stringify(self.form.department),
-              'asv.jobTitle':
-                JSON.stringify(self.form.inputtitle),
-              'asv.website':
-              self.form.inputweb
-            }
-          }).then(data => {
-            self.$message({
-              type: 'success',
-              message: '修改成功'
-            })
-          })
-            .catch(err => {
-              self.$message({
-                type: 'error',
-                message: '修改失败'
-              })
-            })
-        }
-      })
     },
     image(v) {
       if (v) {
         return store.getters.ImageBaseUrl + v
       }
-      return '沒有圖片'
+      return '没有图片'
     },
     updPwd: function() {
       console.log(this.numberValidateForm.newpassw === this.numberValidateForm.oldpassw)
@@ -380,6 +207,12 @@ export default {
           var data = response.data
           if (data.ret === 1) {
             alert('密码修改成功')
+            request({
+              url: '/seller/usr_UsrSupplier_logout',
+              method: 'post'
+            }).then(function() {
+              location.reload()
+            })
           } else {
             alert(data.msg)
           }
